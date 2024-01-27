26b1fa6478c651a56b9c67d10878a1297ed433bf
Hacker00002 <abdulbositsharipovv@gmail.com>
2024-01-27 21:54:40 +0500

    finished header

--- frontend/app/(home)/page.tsx
+++ frontend/app/(home)/page.tsx
@@ -2,8 +2,10 @@ import Container from "@/components/container/container";
 
 export default function Home() {
   return (
-    <Container>
-      <div>Hello</div>
-    </Container>
+    <>
+      <Container>
+        <div></div>
+      </Container>
+    </>
   );
 }
--- frontend/components/navbar-messages.tsx
+++ frontend/components/navbar-messages.tsx
@@ -1,7 +1,36 @@
-import React from "react";
+"use client";
+
+import React, { useState } from "react";
+import { cn } from "@/lib/utils";
+
+import { IoClose } from "react-icons/io5";
 
 const NavbarMessages = () => {
-  return <div></div>;
+  const [showed, setShowed] = useState<boolean>(false);
+
+  const handleShowed = () => {
+    setShowed(true);
+  };
+
+  return (
+    <div className={cn("bg-black py-[8px]", showed && "hidden")}>
+      <div className={"flex items-center justify-center relative"}>
+        <span
+          className={
+            "text-white text-[17px] text-center flex items-center justify-center max-md:text-sm"
+          }
+        >
+          70% discount - Free shipping worldwide
+        </span>
+        <div
+          onClick={handleShowed}
+          className={"absolute right-[16px] cursor-pointer"}
+        >
+          <IoClose color={"white"} size={20} />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default NavbarMessages;
